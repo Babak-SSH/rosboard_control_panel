c53ba84caa65057d7c8075fd82641776a06ac799
Babak-SSH <babak.sefidgar@gmail.com>
2023-11-10 21:26:52 +0330

    Add: add full-screen functionality and button.

--- rosboard/html/js/index.js
+++ rosboard/html/js/index.js
@@ -20,6 +20,46 @@ importJsOnce("js/viewers/GenericViewer.js");
 
 importJsOnce("js/transports/WebSocketV1Transport.js");
 
+  if (document.fullscreenEnabled) {
+    const fullscreen_button = document.createElement("button");
+    fullscreen_button.setAttribute('id','fullscreen-button');
+    fullscreen_button.addEventListener("click", toggle_fullscreen);
+    fullscreen_button.innerHTML  = `
+        <svg viewBox="0 0 24 24">
+            <path d="M7 14H5v5h5v-2H7v-3zm-2-4h2V7h3V5H5v5zm12 
+            7h-3v2h5v-5h-2v3zM14 5v2h3v3h2V5h-5z"/>
+        </svg>
+        <svg viewBox="0 0 24 24">
+            <path d="M5 16h3v3h2v-5H5v2zm3-8H5v2h5V5H8v3zm6 
+            11h2v-3h3v-2h-5v5zm2-11V5h-2v5h5V8h-3z"/>
+        </svg>
+    `;
+    document.body.appendChild(fullscreen_button);
+}
+
+function toggle_fullscreen() {
+    let elem = document.body;
+    if (!document.fullscreenElement) {
+        if (elem.requestFullscreen) {
+          elem.requestFullscreen();
+        } else if (elem.webkitRequestFullscreen) { /* Safari */
+          elem.webkitRequestFullscreen();
+        } else if (elem.msRequestFullscreen) { /* IE11 */
+          elem.msRequestFullscreen();
+        }
+        document.body.setAttribute("fullscreen",""); 
+    } else {
+        if (document.exitFullscreen) {
+          document.exitFullscreen();
+        } else if (document.webkitExitFullscreen) { /* Safari */
+          document.webkitExitFullscreen();
+        } else if (document.msExitFullscreen) { /* IE11 */
+          document.msExitFullscreen();
+        }
+        document.body.removeAttribute("fullscreen"); 
+    }
+} 
+
 var snackbarContainer = document.querySelector('#demo-toast-example');
 
 let subscriptions = {};
@@ -85,7 +125,7 @@ let onOpen = function() {
 let onSystem = function(system) {
   if(system.hostname) {
     console.log("hostname: " + system.hostname);
-    $('.mdl-layout-title').text("ROSboard: " + system.hostname);
+    $('.mdl-layout-title').text("Control Panel: " + system.hostname);
   }
 
   if(system.version) {
