35b57ec8c806635527f9eae71a443c15141b92bd
Babak-SSH <babak.sefidgar@gmail.com>
2024-01-12 23:40:28 +0330

    Add: function to detect device type. Add: function to detect gamepad connectivity and toggle gamepad status. Add: temp text to speech synthesiser.

--- rosboard/html/js/index.js
+++ rosboard/html/js/index.js
@@ -23,6 +23,8 @@ importJsOnce("js/viewers/GenericViewer.js");
 importJsOnce("js/transports/WebSocketV1Transport.js");
 importJsOnce("js/config.js");
 
+importJsOnce("js/Controller.js");
+
 
 // Dont deploy This APi KEY or upload on to github
 const API_KEY = MyApiKey;
@@ -38,6 +40,20 @@ function changeInput(value) {
     const inputElement = document.querySelector('input');
     inputElement.value = value;
 }
+
+let synth = speechSynthesis,
+  isSpeaking = true;
+
+voices();
+
+function voices() {
+  for (let voice of synth.getVoices()) {
+    let selected = voice.name === "Google US English" ? "selected" : "";
+    let option = `<option value="${voice.name}" ${selected}>${voice.name} (${voice.lang})</option>`;
+    voiceList.insertAdjacentHTML("beforeend", option);
+  }
+}
+
 
 async function getMessage() {
         console.log('Success!');
@@ -62,6 +78,43 @@ async function getMessage() {
         const data = await response.clone().json();
         console.log(data);
         outPutElement.textContent = data.choices[0].message.content;
+        // textToSpeech(data.choices[0].message.content);
+    if ('speechSynthesis' in window) {
+    }else{
+      alert("Sorry, your browser doesn't support text to speech!");
+    }
+    console.log("speaking");
+    // var response = new SpeechSynthesisUtterance();
+    // response.text = text;
+    // response.lang = 'fa'
+    // window.speechSynthesis.speak(response);
+
+  // Create a new SpeechSynthesisUtterance object
+  let utterance = new SpeechSynthesisUtterance();
+
+//   utterance.lang = 'fa';
+// utterance.voice = 'fa';
+  // Set the text and voice of the utterance
+  utterance.text = data.choices[0].message.content;
+  console.log(data.choices[0].message.content);
+  for (let voice of synth.getVoices()) {
+    // console.log(voice.name);
+    if (voice.name === "Persian") {
+        console.log(voice.name);
+      utterance.voice = voice;
+    }
+  }
+//   setTimeout(() => {
+//     console.log(window.speechSynthesis.getVoices());
+// }, 50);
+    // const voices = utterance.getVoices();
+    // console.log(voices);
+// utterance.text = "hello there";
+//   utterance.voice = window.speechSynthesis.getVoices()[0];
+
+  // Speak the utterance
+  synth.speak(utterance);
+ 
             if (data.choices[0].message.content && inputElement.value) {
                 const pElement = document.createElement('p');
                 pElement.textContent = inputElement.value;
@@ -72,6 +125,31 @@ async function getMessage() {
     }
 }
 
+function textToSpeech(text) {
+    if ('speechSynthesis' in window) {
+    }else{
+      alert("Sorry, your browser doesn't support text to speech!");
+    }
+
+    console.log("speaking");
+    // var response = new SpeechSynthesisUtterance();
+    // response.text = text;
+    // response.lang = 'fa'
+    // window.speechSynthesis.speak(response);
+
+  // Create a new SpeechSynthesisUtterance object
+  let utterance = new SpeechSynthesisUtterance();
+
+  // Set the text and voice of the utterance
+  utterance.text = text;
+  utterance.lang = 'fa';
+  utterance.voice = window.speechSynthesis.getVoices()[0];
+
+  // Speak the utterance
+  window.speechSynthesis.speak(utterance);
+    
+}
+
 submitButton.addEventListener('click', getMessage);
 
 function clearInput() {
@@ -80,6 +158,20 @@ function clearInput() {
 
 buttonElement.addEventListener('click', clearInput);
 
+const GetDrawer = () => {
+    var drawer = document.querySelector('.mdl-layout');
+    drawer.MaterialLayout.toggleDrawer();
+}
+
+const ToggleTabMenu = () => {
+    var header = document.getElementById("mdl-header");
+    if (header.style.display == "flex") {
+        header.style.display = "none";
+    } else {
+        header.style.display = "flex";
+    }
+}
+
 const GetSpeech = () => {
     console.log("clicked microphone");
     // To enable the SpeechRecognition in Firefox Nightly > 72, go to about:config and switch 
@@ -87,6 +179,7 @@ const GetSpeech = () => {
     const SpeechRecognition = window.speechRecognition || window.webkitSpeechRecognition;
    
     let recognition = new SpeechRecognition();
+    recognition.lang = 'fa-IR'
 
         recognition.onstart = () => {
             console.log("starting listening, speak in microphone");
@@ -104,8 +197,36 @@ const GetSpeech = () => {
          recognition.start();
 }
 
+function mobileCheck() {
+    let check = false;
+    (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+    return check;
+};
+
+function toggleGamepadStatus() {
+    var gt = document.getElementById("gamepad-toggle");
+    if (gt.className == "gamepad-on-icon") {
+        gt.className = "gamepad-off-icon";
+    } else {
+        gt.className = "gamepad-on-icon";
+    }
+};
+
+function changeGamepad() {
+    var virtualJoystickR = document.getElementById("joyRight");
+    var virtualJoystickL = document.getElementById("joyLeft")
+    if (virtualJoystickR.style.display == "none") {
+        virtualJoystickR.style.display = "block";
+        virtualJoystickL.style.display = "block";
+    } else {
+        virtualJoystickR.style.display = "none";
+        virtualJoystickL.style.display = "none";
+    }
+}
+
 if (document.fullscreenEnabled) {
     const fullscreen_button = document.createElement("button");
+    fullscreen_button.classList.add("corner-button")
     fullscreen_button.setAttribute('id','fullscreen-button');
     fullscreen_button.addEventListener("click", toggle_fullscreen);
     fullscreen_button.innerHTML  = `
@@ -123,7 +244,9 @@ if (document.fullscreenEnabled) {
 
 function toggle_fullscreen() {
     let elem = document.body;
+    // let controller = document.getElementById("controller-img")
     if (!document.fullscreenElement) {
+        // controller.style = "controller-img-windowed";
         if (elem.requestFullscreen) {
           elem.requestFullscreen();
         } else if (elem.webkitRequestFullscreen) { /* Safari */
